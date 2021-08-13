3be24dea86f5c6805af7683c294c536433cee4c7
farhamohd <farhamohd.ali@gmail.com>
2021-08-12 20:49:54 -0400

    KeyBoardandmouse selenium auto test

---
+++ ui-components/src/main/java/keyAndmouse/KeyboardAndMouse.java
@@ -0,0 +1,24 @@
+package keyAndmouse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class KeyboardAndMouse {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/mohaseenbagwan/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+        WebElement name = driver.findElement(By.id("name"));
+        name.click();
+
+        name.sendKeys("Farha Kachur");
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+        driver.quit();
+    }
+}
